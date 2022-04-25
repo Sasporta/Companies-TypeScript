b7618dcf04d686c1ab803db51ad928750c791e97
yahav <yahav2011@gmail.com>
2022-04-25 20:02:17 +0300

    added tests scripts

---
+++ tests/companies/api.test.ts
@@ -0,0 +1,59 @@
+describe('companies CRUD requests', () => {
+  describe('get companies request', () => {
+    it('should return 200 status with companies', async () => {
+
+    });
+  });
+
+  describe('post company request', () => {
+    it('should return 201 status with new company', async () => {
+
+    });
+
+    describe('when params invalid', () => {
+      it(`should return 422 status with proper error message`, async () => {
+
+      });
+    });
+  });
+
+  describe('get company request', () => {
+    it('should return 200 status with company', async () => {
+
+    });
+
+    describe('when params invalid', () => {
+      it('should return 404 status with proper error message', async () => {
+
+      });
+    });
+  });
+
+  describe('update company request', () => {
+    it('should return 204 status with updated company', async () => {
+
+    });
+
+    describe('when params invalid', () => {
+      it('should return 404 status with proper error message', async () => {
+
+      });
+
+      it(`should return 422 status with proper error message`, async () => {
+
+      });
+    });
+  });
+
+  describe('delete company request', () => {
+    it('should return 204 status with no content', async () => {
+
+    });
+
+    describe('when params invalid', () => {
+      it('should return 404 status with proper error message', async () => {
+
+      });
+    });
+  });
+});
