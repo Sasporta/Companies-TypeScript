5a8a056dd84d9e041bafe5e07d906c6698e3665e
Sasporta <69085015+Sasporta@users.noreply.github.com>
2022-05-23 09:31:31 +0300

    Improve tests performances (#10)

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from '@jest/types';
 
 export default async (): Promise<Config.InitialOptions> => {
   return {
+    clearMocks: true,
     preset: 'ts-jest',
     testEnvironment: 'node',
     clearMocks: true,
