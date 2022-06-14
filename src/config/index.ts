9af63be82265c1e59713799d597b4a412a118c5f
yahav <yahav2011@gmail.com>
2022-06-14 20:06:49 +0300

    found the solution - deploying

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,8 +9,9 @@ export default {
   db: {
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
+    url: process.env.DATABASE_URL,
+    database: process.env.POSTGRES_DB,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
   },
 };
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,18 +4,18 @@ import config from '.';
 import { Company } from '../entities/Company';
 import { Employee } from '../entities/Employee';
 
-const { db: { host, port, username, password, database } } = config;
+const { db: { database, host, password, port, url, username } } = config;
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  url,
   host,
   port,
   username,
   password,
   database,
   entities: [Company, Employee],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
