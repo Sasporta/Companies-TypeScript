0616e3dbb9efaa6e9b7b3065dc53500034822653
yahav <yahav2011@gmail.com>
2022-06-14 17:26:39 +0300

    added src folder and a postinstall script

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,8 +9,8 @@ export default {
   db: {
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   },
 };
--- src/seeds/companies.ts
+++ src/seeds/companies.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 import { Seeder } from 'typeorm-extension';
 
 import { Company } from '../entities/Company';
-import { existingCompanies } from '../tests/api/__mocks__/entities/CompaniesData';
+import { existingCompanies } from '../tests/api/__mocks__/entities/companiesData';
 export default class CompanySeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
     await dataSource.getRepository(Company).insert(existingCompanies);
