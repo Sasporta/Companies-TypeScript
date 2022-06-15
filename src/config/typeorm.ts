0609465d40ea7a806446f57431027ba9cd61fe69
yahav <yahav2011@gmail.com>
2022-06-15 13:17:05 +0300

    adding src to paths + temp solution for db init

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,16 +6,21 @@ import { Employee } from '../entities/Employee';
 
 const { db: { database, host, password, port, url, username } } = config;
 
-export const dataSource = new DataSource({
+export const dataSource = url
+? new DataSource({
   type: 'postgres',
   url,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: [Company, Employee],
+})
+: new DataSource({
+  type: 'postgres',
   host,
   port,
   username,
   password,
   database,
   entities: [Company, Employee],
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
