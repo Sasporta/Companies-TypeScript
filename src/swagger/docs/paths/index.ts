2cab697d75d25793599b05aaeea691e9271be416
yahav <yahav2011@gmail.com>
2022-09-08 15:20:20 +0300

    adding employeeMetadata doc

--- src/swagger/docs/paths/index.ts
+++ src/swagger/docs/paths/index.ts
@@ -1,9 +1,11 @@
 import employees from './employees';
 import companies from './companies';
+import employeesMetadata from './employeesMetadata';
 
 export default {
 	paths: {
 		...employees,
 		...companies,
+		...employeesMetadata,
 	},
 };
