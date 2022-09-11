cdb1ace4d5857620357c1693bd53e03577f47322
Sasporta <69085015+Sasporta@users.noreply.github.com>
2022-09-11 16:00:43 +0300

    added mongo container + seeding (#60)

---
+++ src/seeds/seed_employeesMetadata.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-undef */
+
+db.employeesMetadata.insertMany([
+  {
+    _id: ObjectId('62ffbaedbb1fc0d37638465c'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: '5c685c36-cad4-44c6-b9cd-cc5eb153fdfe',
+    subordinatesCount: 2,
+    createdAt: '2022-08-19T16:31:41.910+00:00',
+    updatedAt: '2022-08-19T16:31:41.910+00:00',
+  },
+  {
+    _id: ObjectId('62ffea64b796fb2a49cfc0cd'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: '6727011b-f665-469f-888a-9f4c40995d48',
+    subordinatesCount: 2,
+    createdAt: '2022-08-19T16:31:41.910+00:00',
+    updatedAt: '2022-08-19T16:31:41.910+00:00',
+  },
+  {
+    _id: ObjectId('63007a8623493575d6950406'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: '50e8c50a-4ba0-4329-98c1-725025f8cc45',
+    subordinatesCount: 2,
+    createdAt: '2022-08-19T16:31:41.910+00:00',
+    updatedAt: '2022-08-19T16:31:41.910+00:00',
+  },
+  {
+    _id: ObjectId('63006941b0ae3380b4ff242f'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: 'c3a9898b-aa86-4c8c-89a5-5a96a7a0711f',
+    subordinatesCount: 0,
+    createdAt: '2022-08-20T04:55:29.438+00:00',
+    updatedAt: '2022-08-20T04:55:29.438+00:00',
+  },
+  {
+    _id: ObjectId('63006988b0ae3380b4ff2433'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: '47a89e37-aef0-4690-80a3-2383ea7bd285',
+    subordinatesCount: 0,
+    createdAt: '2022-08-20T04:56:40.660+00:00',
+    updatedAt: '2022-08-20T04:56:40.660+00:00',
+  },
+  {
+    _id: ObjectId('63006995b0ae3380b4ff2437'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: '48f91ff6-1cdd-49ca-a382-c7a581342d8c',
+    subordinatesCount: 0,
+    createdAt: '2022-08-20T04:56:53.310+00:00',
+    updatedAt: '2022-08-20T04:56:53.310+00:00',
+  },
+  {
+    _id: ObjectId('63006999b0ae3380b4ff243b'),
+    companyUuid: 'aa7e4bd8-a649-4141-8383-beacd85a4e24',
+    employeeUuid: 'f8293029-ed60-4a84-a581-cb8e64f84553',
+    subordinatesCount: 0,
+    createdAt: '2022-08-20T04:56:57.456+00:00',
+    updatedAt: '2022-08-20T04:56:57.456+00:00',
+  },
+  {
+    _id: ObjectId('630069b7b0ae3380b4ff2440'),
+    companyUuid: 'c7a1d809-399d-4991-ad8e-5803c54a0213',
+    employeeUuid: '5a7848e1-5e4c-4cad-8859-2a782a32b924',
+    subordinatesCount: 0,
+    createdAt: '2022-08-20T04:57:27.293+00:00',
+    updatedAt: '2022-08-20T04:57:27.293+00:00',
+  },
+]);
