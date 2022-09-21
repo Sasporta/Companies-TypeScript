a625765cf6a92b217c88230a5df3205e3c8ff84c
yahav <yahav2011@gmail.com>
2022-09-21 17:01:03 +0300

    small fix for fetching employees with employeeUuid = null

--- src/pgQueries/employees/getAll.ts
+++ src/pgQueries/employees/getAll.ts
@@ -19,19 +19,28 @@ export const getAllEmployeesQuery: GetAllEmployeesQueryFn = (
     .from(Employee, 'employee');
 
   if (companyUuid && managerUuid) {
-    getAllQuery = getAllQuery
-      .innerJoin(Employee, 'manager', 'employee.manager_id = manager.id')
-      .innerJoin(Company, 'company', 'employee.company_id = company.id')
-      .where('manager.uuid = :managerUuid', { managerUuid })
-      .andWhere('company.uuid = :companyUuid', { companyUuid });
+    getAllQuery =
+      managerUuid === 'null'
+        ? getAllQuery
+          .innerJoin(Company, 'company', 'employee.company_id = company.id')
+          .where('company.uuid = :companyUuid', { companyUuid })
+          .andWhere('employee.manager_id is null')
+        : getAllQuery
+          .innerJoin(Employee, 'manager', 'employee.manager_id = manager.id')
+          .innerJoin(Company, 'company', 'employee.company_id = company.id')
+          .where('manager.uuid = :managerUuid', { managerUuid })
+          .andWhere('company.uuid = :companyUuid', { companyUuid });
   } else if (companyUuid) {
     getAllQuery = getAllQuery
       .innerJoin(Company, 'company', 'employee.company_id = company.id')
       .where('company.uuid = :companyUuid', { companyUuid });
   } else if (managerUuid) {
-    getAllQuery = getAllQuery
-      .innerJoin(Employee, 'manager', 'employee.manager_id = manager.id')
-      .where('manager.uuid = :managerUuid', { managerUuid });
+    getAllQuery =
+      managerUuid === 'null'
+        ? getAllQuery.where('employee.manager_id is null')
+        : getAllQuery
+          .innerJoin(Employee, 'manager', 'employee.manager_id = manager.id')
+          .where('manager.uuid = :managerUuid', { managerUuid });
   }
 
   return getAllQuery.limit(limit).getMany();
